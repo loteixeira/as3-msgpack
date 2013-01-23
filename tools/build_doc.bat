18c636b59291c3dded5d8576d5a03020686aa66b
Lucas Teixeira <loteixeira@gmail.com>
2013-01-23 19:02:53 -0200

    documentation tool updated

--- tools/build_doc.bat
+++ tools/build_doc.bat
@@ -1,3 +1,3 @@
 rmdir /Q /S %CD%\doc
 mkdir %CD%\doc
-asdoc -main-title=as3-msgpack0.4.1 -warnings=false -output=%CD%/doc/ -doc-sources+=%CD%/src/ -library-path+=%CD%/lib/as3console.swc
+asdoc -main-title=as3-msgpack1.0.0 -warnings=false -output=%CD%/doc/ -doc-sources+=%CD%/src_lib/ -library-path+=%CD%/lib/as3console.swc
